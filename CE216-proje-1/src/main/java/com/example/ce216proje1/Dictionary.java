4352b8f71e02d2e5ed0b5e12d02dc188e111df46
Gözde <105630115+wanna65@users.noreply.github.com>
2023-03-27 23:01:00 +0300

    Dictionary class methods

--- CE216-proje-1/src/main/java/com/example/ce216proje1/Dictionary.java
+++ CE216-proje-1/src/main/java/com/example/ce216proje1/Dictionary.java
@@ -1,4 +1,27 @@
 package com.example.ce216proje1;
 
 public class Dictionary {
+
+    public Language lang; // Not so sure if a language has a dictionary or dictionary has a language
+    // We can change it later after some discussion
+
+    public static void readDictFile(){} // Let's add more details
+    public static void writeDictFile(){} //when we get how to read files from the website.
+
+    public static String searchWord(String word, Language lang){
+        return word;
+    }
+
+    public static String addWord(String word,Language lang){
+        return word;
+    }
+
+    public static String editWord(String word,Language lang){
+        return word;
+    }
+
+    public static String deleteWord(String word,Language lang){
+        return word;
+    }
+
 }
