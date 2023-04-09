e25acd92dc907f5b96a7ed845a0804d79afbf881
Ozge Ozer <104353749+ozgeozerr@users.noreply.github.com>
2023-04-09 14:36:59 +0300

    new classes

--- CE216-proje-1/src/main/java/com/example/ce216proje1/HelloApplication.java
+++ CE216-proje-1/src/main/java/com/example/ce216proje1/HelloApplication.java
@@ -15,12 +15,15 @@ public class HelloApplication extends Application {
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
-        //ahmet deneme 1234
-        //negar was here
+
+
+
 
     }
 
     public static void main(String[] args) {
         launch();
+
+
     }
 }
