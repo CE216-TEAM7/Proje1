213aea3bd5811fda8cec3633b4a76fb4f9d8d28a
Ososki01 <karisikgaming@gmail.com>
2023-04-09 18:19:31 +0300

    gui part finished !

--- CE216-proje-1/src/main/java/com/example/ce216proje1/GUI.java
+++ CE216-proje-1/src/main/java/com/example/ce216proje1/GUI.java
@@ -1,8 +1,12 @@
 package com.example.ce216proje1;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.text.Text;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -10,14 +14,31 @@ import java.util.ResourceBundle;
 public class GUI implements Initializable {
     @FXML
     public ChoiceBox<String> myChoiceBox;
+    @FXML
+    public Label myLabel;
+    @FXML
+    public Button myButton;
 
     public String[]language={"Turkish","English","French","Italian","Swedish","German","Modern Greek"};
+    public String mainLanguage;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        myChoiceBox.getItems().addAll(language);
+    }
+    public String getLanguage()
+    {
+        mainLanguage = myChoiceBox.getValue();
+        this.mainLanguage = mainLanguage;
+        return mainLanguage;
+    }
 
     public static void displayManual() {
 
     }
 
-    public static void displaySearchResults() {
+    public void displaySearchResults(ActionEvent event) {
+        myLabel.setText(getLanguage());
 
     }
 
@@ -41,8 +62,5 @@ public class GUI implements Initializable {
 
     }
 
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-        myChoiceBox.getItems().addAll(language);
-    }
+
 }
